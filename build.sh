3693f110b3b42cc2d1e2bde2679dc387829647ad
rubenbog <ruben.bog@gmail.com>
2025-09-07 22:39:09 -0300

    fix: add dummy build y yaml

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# build.sh
+echo "Build script placeholder"
+# Instala dependencias y aplica migraciones
+#pip install -r requirements.txt
+#python manage.py collectstatic --noinput
+#python manage.py migrate
